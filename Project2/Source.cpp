af9cdece26ce3a6b64c0cba3d37ed5d1263d0ab7
kuro1307 <56852370+sondo1307@users.noreply.github.com>
2021-08-11 21:55:48 +0700

    BT2 - Fix

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
-
+#include <stdlib.h>
+#include <ctype.h>
 using namespace std;
 
 int GiaiThua(int n)
@@ -11,14 +12,25 @@ int GiaiThua(int n)
 }
 
 void Input(int& a) {
-	while (a<=0)
-	{
+	bool check = false;
+	do {
 		cout << "Enter a positive integer number: ";
 		cin >> a;
-	}
+		if (cin.fail())
+		{
+			check = true;
+			cin.clear(); 
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+		}
+		else
+		{
+			check = false;
+		}
+	} while (a<=0 || check);
 }
+
 int main() {
 	int a;
 	Input(a);
-	cout << GiaiThua(a) << endl;
+	cout << GiaiThua(a);
 }
