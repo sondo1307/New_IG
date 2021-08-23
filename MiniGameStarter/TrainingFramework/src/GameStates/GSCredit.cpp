a14c9d8306b62750eed33b5f1d4aabdbf9e0878c
kuro1307 <56852370+sondo1307@users.noreply.github.com>
2021-08-23 18:07:59 +0700

    Task5

---
+++ MiniGameStarter/TrainingFramework/src/GameStates/GSCredit.cpp
@@ -0,0 +1,103 @@
+#include "GSCredit.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "Model.h"
+#include "Camera.h"
+#include "Font.h"
+#include "Sprite2D.h"
+#include "Sprite3D.h"
+#include "Text.h"
+#include "GameButton.h"
+
+GSCredit::GSCredit()
+{
+}
+
+
+GSCredit::~GSCredit()
+{
+}
+
+
+void GSCredit::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("BG_blue_shop.tga");
+
+	// background
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_background = std::make_shared<Sprite2D>(model, shader, texture);
+	m_background->Set2DPosition((float)Globals::screenWidth / 2, (float)Globals::screenHeight / 2);
+	m_background->SetSize(Globals::screenWidth, Globals::screenHeight);
+
+	// button clode
+	texture = ResourceManagers::GetInstance()->GetTexture("icon_X.tga");
+	std::shared_ptr<GameButton>  button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(Globals::screenWidth - 50, 50);
+	button->SetSize(50, 50);
+	button->SetOnClick([]() {
+		GameStateMachine::GetInstance()->PopState();
+		});
+	m_listButton.push_back(button);
+
+	// text
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("Brightly Crush Shine.otf");
+	m_score = std::make_shared< Text>(shader, font, "Do Tuan Son - PTIT", TextColor::BLACK, 1.0);
+	m_score->Set2DPosition(Globals::screenWidth / 2-100, Globals::screenHeight / 2);
+}
+
+void GSCredit::Exit()
+{
+}
+
+
+void GSCredit::Pause()
+{
+}
+
+void GSCredit::Resume()
+{
+}
+
+
+void GSCredit::HandleEvents()
+{
+}
+
+void GSCredit::HandleKeyEvents(int key, bool bIsPressed)
+{
+}
+
+void GSCredit::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto button : m_listButton)
+	{
+		if (button->HandleTouchEvents(x, y, bIsPressed))
+		{
+			break;
+		}
+	}
+}
+
+void GSCredit::HandleMouseMoveEvents(int x, int y)
+{
+}
+
+void GSCredit::Update(float deltaTime)
+{
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSCredit::Draw()
+{
+	m_background->Draw();
+	m_score->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+}
